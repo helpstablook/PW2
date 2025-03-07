de1ade93c2d639922853dc9df423f446d3bb1dae
helpstablook <hellenjunqueira12@gmail.com>
2025-03-07 23:44:34 +0000

    07-03-2025

---
+++ dogdev/assets/js/script.js
@@ -0,0 +1,21 @@
+//Menu HEADER
+
+//Seleciona o menuOper
+let menuOper = document.querySelector('.menu-oper');
+
+//Seleciona o nav dentro do header
+let nav = document.querySelector('header nav');
+
+//Cria um listener para o evento click do menuOpener
+menuOper.addEventListener('click', ()=>{
+    if(nav.classList.contains('opened')){
+        nav.classList.remove('opened');
+        menuOper.querySelector('.close-icon').style.display='none';
+        menuOper.querySelector('.hamburguer-icon').style.display='flex';
+
+     }else{
+        nav.classList.add('opened');
+        menuOper.querySelector('.close-icon').style.display='flex';
+        menuOper.querySelector('.hamburguer-icon').style.display='none';
+    }
+});
